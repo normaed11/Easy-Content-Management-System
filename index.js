d1134a06bd1ff912fafee09b91e31694b6b9a1fe
normadiaz <normadiazchb@me.com>
2022-03-22 08:27:28 -0500

    created database departments

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 // import inquirer
 const inquirer = require('inquirer')
-// import mysql2
-const mysql = require('mysql2')
+
 //  import console.table
 const cTable = require('console.table')
-// create the connection to database 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'cms'
-});
